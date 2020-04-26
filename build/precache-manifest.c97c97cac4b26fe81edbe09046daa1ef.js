a17c5c42d5a40b8f2a8ee20575c627b915af21df
Sergii Mostepan <sergiimostepan@gmail.com>
2020-04-26 21:59:41 +0300

    made hw 2

---
+++ build/precache-manifest.c97c97cac4b26fe81edbe09046daa1ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a21e74bf4f2d7e39ef472b00d3ea1eb",
+    "url": "/goit-react-hw-02-components/index.html"
+  },
+  {
+    "revision": "691202f23cab53e721c6",
+    "url": "/goit-react-hw-02-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d188daadb1d5ffd6fae0",
+    "url": "/goit-react-hw-02-components/static/css/main.6977efc3.chunk.css"
+  },
+  {
+    "revision": "691202f23cab53e721c6",
+    "url": "/goit-react-hw-02-components/static/js/2.2dfbbc80.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-components/static/js/2.2dfbbc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d188daadb1d5ffd6fae0",
+    "url": "/goit-react-hw-02-components/static/js/main.034a2ed6.chunk.js"
+  },
+  {
+    "revision": "9c1444401a63f6e84d5c",
+    "url": "/goit-react-hw-02-components/static/js/runtime-main.ee25694b.js"
+  }
+]);
